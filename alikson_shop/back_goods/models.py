08b1b2d35b738b1a2aa36f6f1d7534644f074b68
codarsssss <codarsssss@gmail.com>
2023-11-28 18:31:18 +0300

    models

--- alikson_shop/back_goods/models.py
+++ alikson_shop/back_goods/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+
+class Characteristic(models.Model):
+    name = models.CharField(max_length=255)
+    is_primary = models.BooleanField(default=True)
+
+
+class CharacteristicValue(models.Model):
+    characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+
+
+class CategoryCharacteristic(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    characteristics = models.ManyToManyField(CharacteristicValue)
