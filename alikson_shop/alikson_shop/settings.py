4cdd40cfec0860e203b750a8d9b09f91d3a55398
codarsssss <codarsssss@gmail.com>
2023-11-30 16:47:14 +0300

    add postgre

--- alikson_shop/alikson_shop/settings.py
+++ alikson_shop/alikson_shop/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+import dotenv
+
+# Загрузка переменных из окружения
+dotenv.load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ls+vgr^0-yvz-kxf++$$a@8ja1kfdsaljxzuf@1b8xqu&-g5xkb43@^1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.getenv('DEBUG', default=0))
 
-ALLOWED_HOSTS = 'localhost', '127.0.0.1'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
